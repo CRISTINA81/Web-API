4eac6567ed8285842f1378e9053faca9b60dc04a
Cristina <Cristinaoso24@hotmail.com>
2017-12-05 19:01:52 -0500

    Add project files.

---
+++ MvcMovie/Migrations/20171122215333_NickName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+	public partial class NickName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+ 
+            migrationBuilder.AddColumn<string>(
+                name: "NickName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "AspNetUsers");
+			
+        }
+    }
+}
